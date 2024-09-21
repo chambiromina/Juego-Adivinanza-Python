7f3df40b84237c82c5cafb064b1558689d88e14e
Romina Chambi <chambiromina4@gmail.com>
2024-09-21 12:13:02 -0300

    se agrega nueva variable nombre1

--- juego-adivinanza.py
+++ juego-adivinanza.py
@@ -9,13 +9,14 @@ cant_max_intentos = 10
 adivinado = False
 
 print("¡Bienvenido al juego de adivinar el numero secreto")
+nombre1 = input("Indica tu nombre: ")
 
 while not adivinado and cant_intentos < cant_max_intentos:
     Entrada = input("Introduce un numero del 1 al 99: ")
     numero = int(Entrada)
 
     if numero == numero_secreto:
-        print("¡Felicitaciones has adivinado el numero secreto!")
+        print("¡Felicitaciones", (nombre1), "has adivinado el numero secreto!")
         adivinado = True
     elif numero < numero_secreto:
         print("El numero es mayor al ingresado")
@@ -26,4 +27,4 @@ while not adivinado and cant_intentos < cant_max_intentos:
     cant_intentos += 1
 
 if not cant_intentos < cant_max_intentos:
-    print("¡Game Over! No has podido adivinar dentro de los 10 intentos")
+    print("¡Game Over!", (nombre1),  "No has podido adivinar dentro de los 10 intentos")
